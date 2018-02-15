7e4511967b57be9e03c25484c23988ccc706883e
Stephen Tvedt <stvedt@zola.com>
2018-02-15 17:01:27 -0500

    proh-gress

--- src/example-6.js
+++ src/example-6.js
@@ -1,19 +1,15 @@
 const fetch = require('node-fetch');
 const prettyjson = require('prettyjson');
 
-let cat1 = fetch('images/cat1.jpg');
+let cat1 = fetch('https://github.com/stvedt/promises/raw/master/images/cat1.jpg');
 
-let cat2 = fetch('images/cat2.jpg');
+let cat2 = fetch('https://github.com/stvedt/promises/raw/master/images/cat2.jpg');
 
-let cat3 = fetch('images/cat3.jpg');
+let cat3 = fetch('https://github.com/stvedt/promises/raw/master/images/cat3.jpg');
 
-Promise.all([])
+Promise.all([cat1, cat2, cat3])
 .then((response) => {
-
   console.log('response:', response);
-  console.log(response.json());
-  return response.json();
-
 }).then((json) => {
 
   console.log('json', json);
