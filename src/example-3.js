11c1ae244f4999232166c67515f2f6af0063b05a
Stephen Tvedt <stvedt@zola.com>
2018-02-19 23:02:52 -0500

    set up for presenation

--- src/example-3.js
+++ src/example-3.js
@@ -1,3 +1,5 @@
+// Chaining and multiple calls
+
 /* call back */
 function laterCallback(message, callback){
   console.log('callback', message);//success or error
@@ -9,34 +11,34 @@ function laterCallback(message, callback){
 laterCallback(1, () => {
   laterCallback(2, () => {
     laterCallback(3, () => {
-      //all done
+      console.log('all done');
     });
   });
 });
 
 /* promise */
 
-var myPromise = new Promise(function(resolve, reject) {
-  //do something Async
-  setTimeout(() => {
-    if(true) {
-      resolve(1);
-    } else {
-      reject('Failure!');
-    }
-  }, 1000);
+// var myPromise = new Promise(function(resolve, reject) {
+//   //do something Async
+//   setTimeout(() => {
+//     if(true) {
+//       resolve(1);
+//     } else {
+//       reject('Failure!');
+//     }
+//   }, 1000);
 	
-});
+// });
 
-myPromise.then((outcome) => {
-  console.log('promise ', outcome);
-  return 2;
-}).then((outcome) => {
-  console.log('promise ', outcome);
-  return 3;
-}).then((outcome) => {
-  console.log('promise ', outcome);
-  return 'final value';
-}).catch((err) => {
-  console.log('error:', err);
-});
+// myPromise.then((outcome) => {
+//   console.log('promise ', outcome);
+//   return 2;
+// }).then((outcome) => {
+//   console.log('promise ', outcome);
+//   return 3;
+// }).then((outcome) => {
+//   console.log('promise ', outcome);
+//   return 'final value';
+// }).catch((err) => {
+//   console.log('error:', err);
+// });
